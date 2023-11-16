f8d0e10c351cecba90ece38da07574ffb2b2ca5e
lai3392 <141746079+lai3392@users.noreply.github.com>
2023-11-16 21:37:21 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-
-
-function changeCalculatorColor(color, instructionId) {
-    document.querySelector('.calculator').style.background = color;
-    document.getElementById(instructionId).textContent = "Calculator Color Changed!";
-}
